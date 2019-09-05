9813e6a1e3b1183a6b018847a7102b62134cc40d
Lucas Teske <lucas@teske.net.br>
2019-09-05 16:25:52 -0300

    Updated functions in example to private (avoid houndci warnings)

--- _examples/test.go
+++ _examples/test.go
@@ -8,7 +8,7 @@ import (
 var log = slog.Scope("MAIN")
 
 
-func Call0(i slog.Instance, arg0 string) {
+func call0(i slog.Instance, arg0 string) {
     l := i.SubScope("Call0").WithFields(map[string]interface{}{
         "arg0": arg0,
     })
@@ -17,20 +17,20 @@ func Call0(i slog.Instance, arg0 string) {
     l.Done("Finished some work")
     l.Note("Not sure what I'm doing...")
     l.Info("Calling Call1")
-    Call1(l, "call1arg")
+    call1(l, "call1arg")
     l.Done("Exiting")
 }
 
-func Call1(i slog.Instance, huebr string) {
+func call1(i slog.Instance, huebr string) {
     l := i.SubScope("Call1").WithFields(map[string]interface{}{
         "huebr": huebr,
     })
     l.Info("Calling Call2")
-    Call2(l, "abcde")
+    call2(l, "abcde")
     l.Warn("Call 1 finished")
 }
 
-func Call2(i slog.Instance, pop string) {
+func call2(i slog.Instance, pop string) {
     l := i.SubScope("Call2").WithFields(map[string]interface{}{
         "pop": pop,
     })
@@ -46,8 +46,8 @@ func main() {
 
     log.Info("Starting program")
     
-    Call0(log, "MyArg0")
+    call0(log, "MyArg0")
 
-    Call1(log, "Call1Arg")
-    Call2(log, "Call2Arg")
+    call1(log, "Call1Arg")
+    call2(log, "Call2Arg")
 }
